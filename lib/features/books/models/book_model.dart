2800c8c4e04fc44406263ecc1a8e38cef33a17e4
Alkurdy <alkurdy.majid@gmail.com>
2026-07-15 21:32:49 +0300

    update: now we use the toMap and fromMap for CRUD repositories, it makes it cleaner and use fewer code just by adding those two concepts in bookModel.dart

--- lib/features/books/models/book_model.dart
+++ lib/features/books/models/book_model.dart
@@ -15,4 +15,25 @@ class BookModel {
     this.isFinished = false,
   });
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'author': author,
+      'totalPages': totalPages,
+      'currentPage': currentPage,
+      'isFinnished': isFinished ? 1 : 0,
+    };
+  }
+
+  factory BookModel.fromMap(Map<String, dynamic> map) {
+    return BookModel(
+      id: map['id'],
+      title: map['title'],
+      author: map['author'],
+      totalPages: map['totalPages'],
+      currentPage: map['currentPage'],
+      isFinished: map['isFinnished'] == 1,
+    );
+  }
 }
--- lib/features/books/repositories/book_repositeries.dart
+++ lib/features/books/repositories/book_repositeries.dart
@@ -2,15 +2,13 @@ import 'package:book_reminder/core/database/app_database.dart';
 import 'package:book_reminder/features/books/models/book_model.dart';
 
 class BookRepositeries {
+
+  
   Future<void> insertBook(BookModel book) async {
     final db = await AppDatabase.database;
 
-    await db.insert('books', {
-      'title': book.title,
-      'author': book.author,
-      'totalPages': book.totalPages,
-      'currentPage': book.currentPage,
-      'isFinished': book.isFinished ? 1 : 0,
-    });
+    await db.insert('books', book.toMap());  // this is semplified because i used toMap and fromMap in bookModel.dart page 
   }
+
+  
 }
